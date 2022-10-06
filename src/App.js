ce012e7bd0b559917bf3e14caf6613be641fbd3b
nimgnas <sangminyn@gmail.com>
2022-10-06 10:51:17 +0900

    과제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  let nextId = useRef(0);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const onClick = () => {
+    const todo = {
+      id: nextId.current++,
+      body: input,
+    };
+    setTodos([...todos, todo]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="header">
+        <div className="todo-form">
+          <input onChange={onChange} value={input} />
+          <button onClick={onClick}>추가하기</button>
+        </div>
+        <h1>Todo List</h1>
+      </div>
+
+      <div className="todo-container">
+        {todos.map((todo) => (
+          <div className="todo-box" key={todo.id}>
+            {todo.body}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
